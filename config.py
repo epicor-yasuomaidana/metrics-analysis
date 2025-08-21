74094a259b6cdc95b074e93be241b7d3a8eb24eb
Yasuo Ignacio Maidana Perez <yasuo.maidana@epicor.com>
2025-08-21 11:35:12 -0600

    refactor config.py to replace namedtuple with dataclass for Grafana URL inputs and improve URL construction method

--- config.py
+++ config.py
@@ -1,38 +1,37 @@
 from collections import namedtuple
+from dataclasses import dataclass
 
-GrafanaUrlInputs = namedtuple("GrafanaUrlInputs", ["from_ts", "to_ts", "var_scenario", "resource_groups", "names_space"])
 ChromeOptions = namedtuple("ChromeOptions", ["user_data_dir","profile_directory"])
 
+@dataclass
+class GrafanaUrlInputs:
+    from_ts: int
+    to_ts: int
+    var_scenario: str
+    resource_groups: str
+    names_space: str
+    base_url: str = "http://mslab-2024:3000/d/"
+    version: str = "eevf3vhn0308wd/k6-execution-monitoring-with-scenario-filters"
+    org_id: int = 1
 
-def build_grafana_url(grafana_inputs: GrafanaUrlInputs):
-    """
-    Constructs a Grafana dashboard URL with specified parameters.
+    def build_url(self):
+        """
+        Constructs a Grafana dashboard URL with specified parameters.
 
-    Args:
-        grafana_inputs (GrafanaUrlInputs): Named tuple containing all required URL parameters:
-                - from_ts (int): The 'from' timestamp.
-                - to_ts (int): The 'to' timestamp.
-                - var_scenario (str): The scenario filter value.
-                - resource_groups (str): The resource groups value.
-                - names_space (str): The namespace value.
-
-    Returns:
-        str: The complete URL with parameters.
-    """
-    base_url = (
-        "http://mslab-2024:3000/d/eevf3vhn0308wd/k6-execution-monitoring-with-scenario-filters"
-        "?orgId=1"
-        "&var-Workspace="
-        "&var-AKS=aksCluster-EastUS"
-        "&var-ds=beh175ytk7i80c"
-        "&var-sub=KineticQATools"
-        "&var-rg=rgProdSaaSSqlResources-EastUS"
-    )
-    params = (
-        f"&from={grafana_inputs.from_ts}"
-        f"&to={grafana_inputs.to_ts}"
-        f"&var-scenario={grafana_inputs.var_scenario}"
-        f"&var-ResourceGroups={grafana_inputs.resource_groups}",
-        f"&var-Namespace={grafana_inputs.names_space}"
-    )
-    return base_url + "".join(params)
+        Returns:
+            str: The complete URL with parameters.
+        """
+        base_url = self.base_url + self.version + "?orgId=" + str(self.org_id)
+        params = (
+            f"&var-Workspace="
+            f"&var-AKS=aksCluster-EastUS"
+            f"&var-ds=beh175ytk7i80c"
+            f"&var-sub=KineticQATools"
+            f"&var-rg=rgProdSaaSSqlResources-EastUS"
+            f"&from={self.from_ts}"
+            f"&to={self.to_ts}"
+            f"&var-scenario={self.var_scenario}"
+            f"&var-ResourceGroups={self.resource_groups}"
+            f"&var-Namespace={self.names_space}"
+        )
+        return base_url + params
