2bae4b8bb6278b7308afe4872e58749de85df769
Yasuo Ignacio Maidana Perez <yasuo.maidana@epicor.com>
2025-08-21 15:43:03 -0600

    enhance QuickDownloader to skip existing downloads and add forced download option

--- quick_downloader.py
+++ quick_downloader.py
@@ -1,9 +1,9 @@
-from cache_manager.file_manager import get_csv_files_from_download, get_generated_file, move_generated_file
+from cache_manager.file_manager import get_csv_files_from_download, get_generated_file, move_generated_file, skip_search
 from chrome_utils import GrafanaUrlInputs, ChromeDriver, ChromeOptions
 
 default_titles = [
     "Total Requests per Scenario", "TTFB per Scenario",
-    "Iteration per Scenario", "Request Duration per Scenario p99",
+    "Iterations per Scenario", "Request Duration per Scenario p99",
     "Avg Iteration Duration p99"]
 
 
@@ -15,34 +15,36 @@ class QuickDownloader:
         self.dashboards = dashboards
         self.download_path = download_path
 
-    def download(self, test_id: str = None):
+    def download(self, test_id: str = None, forced: bool = False):
         for grafana_url in self.grafana_urls:
             self.grafana_urls = grafana_url
             self.driver.go_to_dashboard(grafana_url.build_url())
             instance = grafana_url.names_space
             starting_date = grafana_url.str_from_ts()
             for title in self.dashboards:
-                print(f"\033[94mHover {{{title}}}\033[0m")
                 original_files = get_csv_files_from_download(self.download_path)
+                destination_path = f"./quick/{instance}_{starting_date}_{test_id}_{title}.csv"
+                if skip_search(destination_path, forced):
+                    print(f"Skipping {title} as it already exists in {destination_path}")
+                    continue
+                print(f"\033[94mHover {{{title}}}\033[0m")
                 self.driver.click_menu_and_inspect_data(title)
                 input(f"Enter to continue after saving {title}...")
                 generated_file = get_generated_file(original_files, self.download_path)
-                destination_path = f"./quick/{instance}_{starting_date}_{test_id}_{title}.csv"
                 move_generated_file(generated_file, destination_path)
                 input("Press Enter to continue...")
 
 
-
 if __name__ == "__main__":
-    from_ts = "now-30m"  # Example timestamp
-    to_ts = "now"  # Example timestamp
+    from_ts = "2025-08-21 13:30:00"  # Example timestamp
+    to_ts = "2025-08-21 15:00:00"  # Example timestamp
     var_scenario = "ARInvoiceTracker"
     resource_groups = "rgQAToolsSaaSAKSResources-EastUS"
     names_space = "perfwamd2"
 
     grafana_inputs = GrafanaUrlInputs(from_ts, to_ts, var_scenario, resource_groups, names_space)
     options_ = ChromeOptions(r"C:\Users\yasuo.maidana\AppData\Local\Google\Chrome\User Data\Default",
-                            "Default")
+                             "Default")
 
     downloader = QuickDownloader([grafana_inputs], options_)
     downloader.download()
