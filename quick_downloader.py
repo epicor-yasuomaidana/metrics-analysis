74347e82f7cd0264526695a078b278a4d0f1f959
Yasuo Ignacio Maidana Perez <yasuo.maidana@epicor.com>
2025-08-22 13:43:06 -0600

    add methods to download tables and dashboards in QuickDownloader

--- quick_downloader.py
+++ quick_downloader.py
@@ -2,20 +2,28 @@ from cache_manager.file_manager import get_csv_files_from_download, get_generate
 from chrome_utils import GrafanaUrlInputs, ChromeDriver, ChromeOptions
 
 default_titles = [
-    "Total Requests per Scenario", "TTFB per Scenario",
-    "Iterations per Scenario", "Request Duration per Scenario p99",
+    "Total Requests per Scenario", "Iterations per Scenario",
+    "TTFB per Scenario","Request Duration per Scenario p99",
     "Avg Iteration Duration p99"]
+
+tables_titles = ["Iterations per Scenario", "Request Duration per Scenario p99", "Avg Iteration Duration p99",
+                 "TTFB per Scenario", "Total Requests per Scenario"]
 
 
 class QuickDownloader:
     def __init__(self, grafana_urls: list[GrafanaUrlInputs], options: ChromeOptions,
-                 dashboards: tuple[str, ...] = tuple(default_titles), download_path: str = None):
+                 dashboards: tuple[str, ...] = tuple(default_titles), tables=tuple(tables_titles),
+                 download_path: str = None):
         self.grafana_urls = grafana_urls
         self.driver = ChromeDriver(grafana_urls[0], options=options)
         self.dashboards = dashboards
         self.download_path = download_path
+        self.tables = tables
 
-    def download(self, test_id: str = None, forced: bool = False):
+
+
+    def download_dashboards(self, test_id: str = None, forced: bool = False):
+        print("\033[93mDownloading dashboards...\033[0m")
         for grafana_url in self.grafana_urls:
             self.grafana_urls = grafana_url
             self.driver.go_to_dashboard(grafana_url.build_url())
@@ -34,9 +42,31 @@ class QuickDownloader:
                 print(f"\033[94mHover {{{title}}}\033[0m")
                 self.driver.click_menu_and_inspect_data(title)
                 input(f"Enter to continue after saving {title}...")
+                self.driver.press_key('\u001b')
                 generated_file = get_generated_file(original_files, self.download_path)
                 move_generated_file(generated_file, destination_path)
-                input("Press Enter to continue...")
+
+    def download_tables(self, test_id: str = None, forced: bool = False):
+        print("\033[93mDownloading tables...\033[0m")
+        for grafana_url in self.grafana_urls:
+            self.driver.go_to_dashboard(grafana_url.build_url())
+            instance = grafana_url.names_space
+            starting_date = grafana_url.str_from_ts()
+            input(f"Enter to continue ...")
+            for title in self.tables:
+                if test_id:
+                    destination_path = f"./quick/{instance}_{starting_date}t_id{test_id}_{title}_table.csv"
+                else:
+                    destination_path = f"./quick/{instance}_{starting_date}{title}_table.csv"
+
+                print("Getting table data for:", title)
+
+                if skip_search(destination_path, forced):
+                    print(f"Skipping {title} as it already exists in {destination_path}")
+                    continue
+                print(f"\033[94mSaving {{{title}}} table\033[0m")
+                table_data = self.driver.copy_table(title)
+                table_data.to_csv(destination_path, index=False, encoding="utf-8")
 
 
 if __name__ == "__main__":
@@ -45,19 +75,14 @@ if __name__ == "__main__":
     var_scenario = "ARInvoiceTracker"
     resource_groups = "rgQAToolsSaaSAKSResources-EastUS"
     names_space = "perfwamd2"
-
-    dashboards_ = ("Total Requests per Scenario", "TTFB per Scenario")
     grafana_inputs = GrafanaUrlInputs(from_ts, to_ts, names_space, var_scenario, resource_groups)
 
     options_ = ChromeOptions(r"C:\Users\yasuo.maidana\AppData\Local\Google\Chrome\User Data\Default",
                              "Default")
 
-    downloader = QuickDownloader([grafana_inputs], options_, dashboards=dashboards_)
-    downloader.download()
-
-    downloader.dashboards = ["Panel Title"]
-    downloader.grafana_urls = [GrafanaUrlInputs(from_ts, to_ts, names_space, version="cevpr06yrc0e8a/group-duration")]
-    downloader.download()
+    downloader = QuickDownloader([grafana_inputs], options_)
+    downloader.download_tables()
+    downloader.download_dashboards()
     input("Press Enter to finish...")
 
     print("Finished")
