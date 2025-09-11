4f698995a4a71020ab57b8e59b8d9959d5afa588
Yasuo Ignacio Maidana Perez <yasuo.maidana@epicor.com>
2025-09-11 12:54:44 -0600

    Add plot_bars function for enhanced bar plotting capabilities

--- plotters/bar_plotters.py
+++ plotters/bar_plotters.py
@@ -31,6 +31,28 @@ def plot_bar(df: DataFrame, x_col: str, y_col: str, title: str, ylabel: str, gro
         plt.show()
 
 
+def plot_bars(df: DataFrame, to_plot: list[str] = None, summarizer: str = "max", grouper: str = "instance",
+              ax: plt.Axes | None = None, title: str = "", ylabel: str = "", formatter: str = "%.0f",
+              bar_label_rotation_padding: tuple[int, int] = (45, 3),
+              axis_params: tuple[str, dict] = ("x", {"rotation": 45})):
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(22, 5))
+
+    processing = df[[grouper] + to_plot].groupby(grouper).agg(summarizer).reset_index()
+    melted = processing.melt(id_vars=grouper, value_vars=tuple(to_plot), var_name="scenario", value_name="value")
+    ax = sns.barplot(data=melted, x="scenario", y="value", hue=grouper, ax=ax)
+    for container in ax.containers:
+        if isinstance(container, BarContainer):
+            ax.bar_label(container, fmt=formatter, rotation=bar_label_rotation_padding[0],
+                         padding=bar_label_rotation_padding[1])
+    for axis, params in (axis_params,):
+        ax.tick_params(axis='x', **params)
+
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+
+
 def plot_cat_bar(
         df: DataFrame,
         y_col: str,
