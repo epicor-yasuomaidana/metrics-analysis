3771213e3f031c9af9c46536c5eef90ea4f0d4f8
Yasuo Ignacio Maidana Perez <yasuo.maidana@epicor.com>
2025-09-11 12:04:11 -0600

    Enhance load.py with new data processing functions and improve CSV loading

--- processor/load.py
+++ processor/load.py
@@ -1,3 +1,5 @@
+from typing import Callable
+import re
 import pandas as pd
 from pandas import DataFrame
 
@@ -7,7 +9,6 @@ def cast_string_duration_to_seconds(series: pd.Series) -> pd.Series:
         if pd.isnull(value):
             return pd.NA
 
-        import re
         total_seconds = 0.0
         matches = re.findall(r'([\d.]+)\s*(s|ms)', value)
         for amount, unit in matches:
@@ -20,11 +21,11 @@ def cast_string_duration_to_seconds(series: pd.Series) -> pd.Series:
     return series.apply(parse_duration)
 
 
-def load_normalized_csv(file_path: str) -> DataFrame:
-    df = pd.read_csv(file_path, parse_dates=['Time'])
+def load_normalized_csv(file_path: str, time_column: str = "Time") -> DataFrame:
+    df = pd.read_csv(file_path, parse_dates=[time_column])
     df.Time -= df.Time.min()
-    df["dT"] = df["Time"].diff().dt.total_seconds()
-    df.set_index('Time', inplace=True)
+    df["dT"] = df[time_column].diff().dt.total_seconds()
+    df.set_index(time_column, inplace=True)
     df.sort_index(inplace=True)
     return df
 
@@ -39,12 +40,11 @@ def load_with_caster(file_path, columns_to_cast=None, caster=cast_string_duratio
     return df
 
 
-
-
-def load_sum_total(file_path, columns_to_sum, caster=cast_string_duration_to_seconds) -> DataFrame:
+def load_sum_total(file_path: str, columns_to_sum: list[str], caster=cast_string_duration_to_seconds) -> DataFrame:
     df = load_with_caster(file_path, columns_to_cast=columns_to_sum, caster=caster)
     df["total"] = df[columns_to_sum].sum(axis=1)
     return df
+
 
 def load_mean_df(file_path, columns_to_average, caster=cast_string_duration_to_seconds) -> DataFrame:
     df = load_with_caster(file_path, columns_to_cast=columns_to_average, caster=caster)
@@ -52,10 +52,39 @@ def load_mean_df(file_path, columns_to_average, caster=cast_string_duration_to_s
     return df
 
 
-if __name__ == '__main__':
-    # file_path_ = "../quick/perflamd1_2025-08-22_12-50-00-Avg Iteration Duration p99.csv"
-    # normalized_df = load_normalized_csv(file_path_)
-    # print(normalized_df.head())
-    file_path_ ="../quick/perflamd1_2025-08-22_12-50-00-TTFB per Scenario.csv"
-    sum_total_df = load_sum_total(file_path_)
-    print(sum_total_df.head())
+def load_rates_per_scenario(path: str, scenarios, rate_columns: tuple[str, ...] = ("total",)) -> pd.DataFrame:
+    df = load_sum_total(path, scenarios)
+    for col in rate_columns:
+        df[f"{col}_rate"] = df[col].diff() / df["dT"]
+    return df
+
+
+def get_os_and_processor(name: str) -> tuple[str, str]:
+    os_ = "Windows" if "perfw" in name.lower() else "Linux"
+    processor = "AMD" if "amd" in name.lower() else "Intel"
+    return os_, processor
+
+
+def merge_dashboards(stored_data: dict[str, dict], dashboard_name: str, loader: Callable,
+                     scenarios: list[str] = None) -> pd.DataFrame:
+    if scenarios is None:
+        scenarios = ['BAQ', 'PartMaintenance', 'PartTracker', 'CustomerTracker',
+                     'SalesOrderTracker', 'CopySalesOrder', 'POEntry20',
+                     'MiscARInvoice', 'SOEntry10ShipTos', 'POEntry10', 'ProcessMRP',
+                     'QuoteEntry', 'GLJournalTracker', 'QuantityOnHandCheck',
+                     'SalesOrderEntry', 'ARInvoiceTracker', 'SalesOrderErrorNO',
+                     'JobTracker', 'SalesOrderErrorYES']
+    dfs = []
+    for data in stored_data.values():
+        vus = data["vus"]
+        test_instance, identifier = data["instance_identifier"]
+        dashboards = data["dashboards"]
+
+        df = loader(dashboards[dashboard_name], scenarios)
+        df["vus"] = vus
+        df["instance"] = "{} {}".format(*get_os_and_processor(test_instance))
+        df["identifier"] = identifier
+        df["os"], df["processor"] = get_os_and_processor(test_instance)
+        dfs.append(df)
+
+    return pd.concat(dfs).sort_index()
